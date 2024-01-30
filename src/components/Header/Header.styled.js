c79a3eb77cb32f3277ff40b612f15d1c79c9d880
degull <skytmdgus99@gmail.com>
2024-01-30 14:58:38 +0900

    header 수정

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,38 +1,35 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-   width:100%;
-   box-shadow:0 1.5px 0 2px rgba(0,0,0, 0.03);
-   position: relative;
-   z-index: 3;
-   margin: 0;
-   padding: 0;
+  width: 100%;
+  box-shadow: 0 1.5px 0 2px rgba(0, 0, 0, 0.03);
+  position: relative;
+  z-index: 3;
+  margin: 0;
+  padding: 0;
 `;
 
-
 export const Header = styled.div`
-   width: 1300px;
-   height: 70px;
-   margin: 0 auto;
-   position: relative;
-   left: 0;
+  width: 1300px;
+  height: 70px;
+  margin: 0 auto;
+  position: relative;
+  left: 0;
 `;
 
 export const Modal = styled.div`
-   position: absolute;
-   top: 20px;
-   right: 30px;
+  position: absolute;
+  top: 20px;
+  right: 30px;
 `;
 
 export const Logo = styled.img`
-   position: absolute;
-   width: 150px;
-   left: -80px;
-   bottom: 40px;
-   cursor: pointer;
+  position: absolute;
+  width: 150px;
+  left: -80px;
+  bottom: 20px;
+  cursor: pointer;
 `;
-
-
 
 export const MenuBar = styled.div`
   /* z-index: 3;
@@ -48,124 +45,118 @@ export const MenuBar = styled.div`
   top: 70px;
 `;
 
-
 export const Menu = styled.div`
-   position: relative;
-   display: flex;
-   height: 40px;
-   justify-content:center;
-   align-items: center;
-   margin: 50px 0 0 80px;
+  position: relative;
+  display: flex;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  margin: 50px 0 0 80px;
 `;
 
-export const SubMenu = styled.div`
-   
-`;
+export const SubMenu = styled.div``;
 
 export const MenuStyle1 = styled.div`
-   font: bold 13px/1 "arial";
-   background-color: #fff;
-   color: #555;
-   width: 123px;
-   padding: 20px 40px;
-   transition: all 0.5s;
-   position: relative;
-   padding: 20px 40px;
-   position: relative;
-   cursor: pointer;  
-   left: 0px; 
-   line-height: 3rem;
-   /* background: rgba(153, 153, 153, 0); */
-   
-`
+  font: bold 13px/1 "arial";
+  background-color: #fff;
+  color: #555;
+  width: 200px;
+  padding: 20px 40px;
+  transition: all 0.5s;
+  position: relative;
+  padding: 20px 40px;
+  position: relative;
+  cursor: pointer;
+  left: 0.8%;
+  line-height: 3rem;
+  /* background: rgba(153, 153, 153, 0); */
+`;
 
 export const MenuStyle2 = styled.div`
-   font: bold 13px/1 "arial";
-   background-color: #fff;
-   color: #555;
-   width: 123px;
-   padding: 20px 40px;
-   transition: all 0.5s;
-   position: relative;
-   padding: 20px 40px;
-   position: relative;
-   cursor: pointer;  
-   left: 12.7rem;
-   line-height: 3rem;
-`
+  font: bold 13px/1 "arial";
+  background-color: #fff;
+  color: #555;
+  width: 200px;
+  padding: 20px 40px;
+  transition: all 0.5s;
+  position: relative;
+  padding: 20px 40px;
+  position: relative;
+  cursor: pointer;
+  left: 13.1rem;
+  line-height: 3rem;
+`;
 export const MenuStyle3 = styled.div`
-   font: bold 13px/1 "arial";
-   background-color: #fff;
-   width: 123px;
-   color: #555;
-   padding: 20px 40px;
-   transition: all 0.5s;
-   position: relative;
-   padding: 20px 40px;
-   position: relative;
-   cursor: pointer;  
-   left: 25.5rem;
-   line-height: 3rem;
-`
+  font: bold 13px/1 "arial";
+  background-color: #fff;
+  width: 200px;
+  color: #555;
+  padding: 20px 40px;
+  transition: all 0.5s;
+  position: relative;
+  padding: 20px 40px;
+  position: relative;
+  cursor: pointer;
+  left: 25.6rem;
+  line-height: 3rem;
+`;
 
 export const MenuStyle4 = styled.div`
-   font: bold 13px/1 "arial";
-   color: #555;
-   padding: 20px 40px;
-   background-color: #fff;
-   width: 123px;
-   transition: all 0.5s;
-   position: relative;
-   padding: 20px 40px;
-   position: relative;
-   cursor: pointer;  
-   left: 38.1rem;
-   line-height: 3rem;
-`
+  font: bold 13px/1 "arial";
+  color: #555;
+  padding: 20px 40px;
+  background-color: #fff;
+  width: 200px;
+  transition: all 0.5s;
+  position: relative;
+  padding: 20px 40px;
+  position: relative;
+  cursor: pointer;
+  left: 38.1rem;
+  line-height: 3rem;
+`;
 
 export const MenuStyle5 = styled.div`
-   font: bold 13px/1 "arial";
-   background-color: #fff;
-   width: 123px;
-   color: #555;
-   padding: 20px 40px;
-   transition: all 0.5s;
-   position: relative;
-   padding: 20px 40px;
-   position: relative;
-   cursor: pointer;  
-   left: 50.8rem;
-   line-height: 3rem;
-`
+  font: bold 13px/1 "arial";
+  background-color: #fff;
+  width: 200px;
+  color: #555;
+  padding: 20px 40px;
+  transition: all 0.5s;
+  position: relative;
+  padding: 20px 40px;
+  position: relative;
+  cursor: pointer;
+  left: 50.6rem;
+  line-height: 3rem;
+`;
 
 export const MenuStyle6 = styled.div`
-   font: bold 13px/1 "arial";
-   color: #555;
-   background-color: #fff;
-   width: 123px;
-   padding: 20px 40px;
-   transition: all 0.5s;
-   position: relative;
-   padding: 20px 40px;
-   position: relative;
-   cursor: pointer;  
-   left: 63.5rem;
-   line-height: 3rem;
-`
-
-
+  font: bold 13px/1 "arial";
+  color: #555;
+  background-color: #fff;
+  width: 200px;
+  padding: 20px 40px;
+  transition: all 0.5s;
+  position: relative;
+  padding: 20px 40px;
+  position: relative;
+  cursor: pointer;
+  left: 63.2rem;
+  line-height: 3rem;
+`;
 
 export const SubMenu1 = styled.div`
-   /* font: bold 13px/1 "arial";
+  /* font: bold 13px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative;
    cursor: pointer; */
-   /* margin-left: 4rem; */
+  /* margin-left: 4rem; */
 `;
 export const SubMenu2 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 0px;
    transition: all 0.5s;
@@ -176,32 +167,32 @@ export const SubMenu2 = styled.div`
 `;
 
 export const SubMenu3 = styled.div`
-   /* font: bold 15px/1 "arial";
+  /* font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   // transform: translateX(4rem);
+  // transform: translateX(4rem);
 `;
 
 export const SubMenu4 = styled.div`
-   /* font: bold 15px/1 "arial";
+  /* font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   // margin-left: 4rem;
+  // margin-left: 4rem;
 `;
 export const SubMenu5 = styled.div`
-   /* font: bold 15px/1 "arial";
+  /* font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   // margin-left: 4rem;
+  // margin-left: 4rem;
 `;
 export const SubMenu6 = styled.div`
-   /* font: bold 15px/1 "arial";
+  /* font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
@@ -209,7 +200,7 @@ export const SubMenu6 = styled.div`
    */
 `;
 export const SubMenu7 = styled.div`
-   /* font: bold 15px/1 "arial";
+  /* font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
@@ -217,58 +208,53 @@ export const SubMenu7 = styled.div`
     */
 `;
 export const SubMenu8 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
 `;
 export const SubMenu9 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
 `;
 export const SubMenu10 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
- 
 `;
 export const SubMenu11 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu12 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu13 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu14 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu15 = styled.div`
   /*    font: bold 15px/1 "arial";
@@ -276,127 +262,121 @@ export const SubMenu15 = styled.div`
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu16 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-  
 `;
 export const SubMenu17 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu18 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu19 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 export const SubMenu20 = styled.div`
-/*    font: bold 15px/1 "arial";
+  /*    font: bold 15px/1 "arial";
    color: #555;
    padding: 20px 40px;
    transition: all 0.5s;
    position: relative; */
-   
 `;
 
 export const Menu1 = styled.div`
-   width: 85px;
-   font: bold 15px/1 "arial";
-   color: #555;
-   padding: 20px 60px;
-   transition: all 0.5s;
-   position: relative;
-   cursor: pointer;
-   &:hover{
-      border-bottom: 3px solid #0D67BB;
-      transition: all 0.1s;
-   }
-   text-align: center;
+  width: 200px;
+  font: bold 15px/1 "arial";
+  color: #555;
+  padding: 31px 0px;
+  transition: all 0.5s;
+  position: relative;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 3px solid #0d67bb;
+    transition: all 0.1s;
+  }
+  text-align: center;
 `;
 
 export const Menu2 = styled.div`
-   width: 85px;
-   font: bold 15px/1 "arial";
-   color: #555;
-   padding: 20px 60px;
-   transition: all 0.5s;
-   cursor: pointer;
-   &:hover{
-      border-bottom: 3px solid #0D67BB;
-      transition: all 0.1s;
-   }
-   text-align: center;
+  width: 200px;
+  font: bold 15px/1 "arial";
+  color: #555;
+  padding: 31px 0px;
+  transition: all 0.5s;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 3px solid #0d67bb;
+    transition: all 0.1s;
+  }
+  text-align: center;
 `;
 
 export const Menu3 = styled.div`
-   width: 85px;
-   font: bold 15px/1 "arial";
-   color: #555;
-   padding: 20px 60px;
-   transition: all 0.1s;
-   cursor: pointer;
-   &:hover{
-      border-bottom: 3px solid #0D67BB;
-      transition: all 0.1s;
-   }
-   text-align: center;
+  width: 200px;
+  font: bold 15px/1 "arial";
+  color: #555;
+  padding: 31px 0px;
+  transition: all 0.1s;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 3px solid #0d67bb;
+    transition: all 0.1s;
+  }
+  text-align: center;
 `;
 export const Menu4 = styled.div`
-   width: 85px;  
-   font: bold 15px/1 "arial";
-   color: #555;
-   padding: 20px 60px;
-   transition: all 0.5s;
-   cursor: pointer;
-   &:hover{
-      border-bottom: 3px solid #0D67BB;
-      transition: all 0.1s;
-   }
-   text-align: center;
+  width: 200px;
+  font: bold 15px/1 "arial";
+  color: #555;
+  padding: 31px 0px;
+  transition: all 0.5s;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 3px solid #0d67bb;
+    transition: all 0.1s;
+  }
+  text-align: center;
 `;
 export const Menu5 = styled.div`
-   width: 85px;
-   font: bold 15px/1 "arial";
-   color: #555;
-   padding: 20px 60px;
-   transition: all 0.5s;
-   cursor: pointer;
-   &:hover{
-      border-bottom: 3px solid #0D67BB;
-      transition: all 0.1s;
-   }
-   text-align: center;
+  width: 200px;
+  font: bold 15px/1 "arial";
+  color: #555;
+  padding: 31px 0px;
+  transition: all 0.5s;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 3px solid #0d67bb;
+    transition: all 0.1s;
+  }
+  text-align: center;
 `;
 export const Menu6 = styled.div`
-   width: 85px; 
-   font: bold 15px/1 "arial";
-   color: #555;
-   padding: 20px 60px;
-   transition: all 0.5s;
-   cursor:pointer;
-   &:hover{
-      border-bottom: 3px solid #0D67BB;
-      transition: all 0.1s;
-   }
-   text-align: center;
+  width: 200px;
+  font: bold 15px/1 "arial";
+  color: #555;
+  padding: 31px 0px;
+  transition: all 0.5s;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 3px solid #0d67bb;
+    transition: all 0.1s;
+  }
+  text-align: center;
 `;
