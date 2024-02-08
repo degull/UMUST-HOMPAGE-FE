483efaa2bf82637f3364e9c65312b525ad877525
degull <skytmdgus99@gmail.com>
2024-02-09 06:56:16 +0900

    카카오맵 api 수정

--- src/etc/Map/Map.js
+++ src/etc/Map/Map.js
@@ -5,41 +5,56 @@ import * as S from './Map.styled';
 
 const Map = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=4d2258a550a5cc13b15f5bcf9bf34124';
-    document.head.appendChild(script);
+    const loadMapScript = () => {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=4d2258a550a5cc13b15f5bcf9bf34124';
+      script.onload = () => {
+        if (window.kakao) {
+          mapScript();
+        } else {
+          console.error('카카오 맵 API 스크립트 로드됨, 그러나 window.kakao가 정의되지 않았습니다.');
+        }
+      };
+      script.onerror = () => {
+        console.error('카카오 맵 API 스크립트 로드 중 오류가 발생했습니다.');
+      };
+      document.head.appendChild(script);
+    };
 
-    script.onload = () => {
-      // kakao 객체가 로드된 후 mapscript 호출
-      if (window.kakao) {
-        mapscript();
+    const mapScript = () => {
+      const container = document.getElementById('map');
+      if (container) {
+        const options = {
+          center: new window.kakao.maps.LatLng(37.6544068, 127.0497957),
+          level: 5,
+        };
+
+        const map = new window.kakao.maps.Map(container, options);
+
+        const markerdata = [
+          { lat: 37.6544068, lng: 127.0497957, title: '(주)유머스트알엔디' },
+        ];
+
+        markerdata.forEach((el) => {
+          const marker = new window.kakao.maps.Marker({
+            map: map,
+            position: new window.kakao.maps.LatLng(el.lat, el.lng),
+            title: el.title,
+          });
+
+          // (선택 사항) 마커에 이벤트 리스너 추가
+          window.kakao.maps.event.addListener(marker, 'click', () => {
+            // 마커 클릭 시 수행할 작업 추가
+          });
+        });
+      } else {
+        console.error('맵 컨테이너를 찾을 수 없습니다.');
       }
     };
+
+    loadMapScript();
   }, []);
-
-  const mapscript = () => {
-    let container = document.getElementById("map");
-    let options = {
-      center: new window.kakao.maps.LatLng(37.6544068, 127.0497957),
-      level: 5,
-    };
-
-    const map = new window.kakao.maps.Map(container, options);
-
-    
-    const markerdata = [
-      { lat: 37.6544068, lng: 127.0497957, title: '(주)유머스트알엔디' },
-    ];
-
-    markerdata.forEach((el) => {
-      new window.kakao.maps.Marker({
-        map: map,
-        position: new window.kakao.maps.LatLng(el.lat, el.lng),
-        title: el.title,
-      });
-    });
-  };
 
   return (
     <S.Wrapper>
@@ -47,7 +62,7 @@ const Map = () => {
       <S.WrapImage src="/img/test.png" />
       <S.TitleImg src='/img/map_te.png'/>
       <S.MainComponent src="/img/map_k.png" />
-      <S.MapContainer id="map" style={{ width: "800px", height: "350px", margin:"0 auto", marginBottom:"30px"}} />
+      <S.MapContainer id="map" style={{ width: '800px', height: '350px', margin: '0 auto', marginBottom: '30px' }} />
       <Footer />
     </S.Wrapper>
   );
